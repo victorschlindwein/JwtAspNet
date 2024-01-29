955e732b7c73d986581524b5094fe1144ea05eeb
dizzysch <victorwilbert@gmail.com>
2024-01-29 10:13:39 -0300

    feat: add authorization

--- Program.cs
+++ Program.cs
@@ -1,16 +1,35 @@
+using System.Text;
+using JwtAspNet;
 using JwtAspNet.Models;
 using JwtAspNet.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddTransient<TokenService>();
-builder.Services.AddAuthentication();
+
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+    .AddJwtBearer(x =>
+{
+    x.TokenValidationParameters = new TokenValidationParameters
+    {
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(Configuration.PrivateKey)),
+        ValidateIssuer = false,
+        ValidateAudience = false
+    };
+});
+
 builder.Services.AddAuthorization();
 
 var app = builder.Build();
 app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapGet("/", (TokenService service) =>
+app.MapGet("/login", (TokenService service) =>
 {
     var user = new User(
         1,
@@ -20,8 +39,10 @@ app.MapGet("/", (TokenService service) =>
         "rova10",
         ["student", "premium"]
     );
-    
+
     return service.Create(user);
 });
+
+app.MapGet("/authorized", () => "Acesso autorizado").RequireAuthorization();
 
 app.Run();
